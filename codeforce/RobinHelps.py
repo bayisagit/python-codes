928d699d11380b949f7d912fcac65f6827ee9d58
Bayisa Daba <bayisa615@gmail.com>
2024-10-02 20:34:17 +0300

    modify

--- codeforce/RobinHelps.py
+++ codeforce/RobinHelps.py
@@ -1,25 +1,15 @@
-def robin(k,testc):
-    rek=[]
-    for case in testc:
-        n,m,gold=case
-        robg=0
-        robgi=0
-        for g in gold:
-            if g>=m:
-                robg+=g
-            elif g==0 and robg>0:
-                robg-=1
-                robgi+=1
-        rek.append(robgi)
-    return rek
-        
-k=int(input().strip())
-testc=[]
-for i in range(k):
-    n, m=map(int,input().strip().split())
-    gold=list(map(int,input().strip().split()))
-    testc.append((n,m,gold))
-results=robin(k,testc)
-for res in results:
-    print(res)
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    arr = list(map(int, input().split()))
     
+    rob = 0
+    cnt = 0
+    for num in arr:
+        if num >= k:
+            rob += num
+        if num == 0 and rob > 0:
+            cnt += 1
+            rob -= 1
+    print(cnt)
